b42caef2face484dd9beb628ba1fcfdf749ac2f2
Rizwan <rizwanrensa@gmail.com>
2019-10-27 16:11:17 -0400

    added all the books controllers

---
+++ controllers/booksController.js
@@ -0,0 +1,31 @@
+// get the database models
+const db = require('../models')
+
+module.exports={
+  findAll(req,res){
+    db.Book
+    .find(req.query)
+    .sort({date:-1})
+    .then(dbModel=>res.json(dbModel))
+    .catch(err=>res.status(422).json(err))
+  },
+  findById(req,res){
+    db.Book
+    .findById({ _id: req.params.id })
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err))
+  },
+  create(req,res){
+    db.Book
+    .create(req.body)
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err))
+  },
+  remove(req,res){
+    db.Book
+      .findById({ _id: req.params.id })
+      .then(dbModel=>dbModel.remove())
+      .then(dbModel =>res.json(dbModel))
+      .catch(err=>res.status(422).json(err))
+  }
+}
