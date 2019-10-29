39d41a59fe0479f2059aad3ba90be05d4f04f04d
Rizwan <rizwanrensa@gmail.com>
2019-10-28 22:20:40 -0400

    added the mongoose heroku db link

---
+++ client/build/precache-manifest.69650dcfeef34c44482124e904f680e7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b74b59303a545518c070",
+    "url": "/static/js/main.00431991.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f47dbc4a82e274ceb668",
+    "url": "/static/js/2.12754193.chunk.js"
+  },
+  {
+    "revision": "56108f311bc6a733a73c51dba317be91",
+    "url": "/index.html"
+  }
+];
